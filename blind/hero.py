ef5d0fcca99ceb7cb0d3394b13d6e8d35f5aa1a8
Chris Burnham <chris@burnhamup.com>
2015-01-27 21:39:53 -0800

    Changing movement type to better accomodate sound

--- blind/hero.py
+++ blind/hero.py
@@ -1,3 +1,5 @@
+import math
+import pygame
 from pygame.sprite import Sprite, spritecollideany
 from blind.util import load_image
 
@@ -14,27 +16,43 @@ class Hero(Sprite):
         self.image = load_image("placeholder.gif")
         self.rect = self.image.get_rect()
         self.rect.center = (100, 100)
+        self.x_float, self.y_float = self.rect.x, self.rect.y
         self.directions = {direction: False for direction in Hero.DIRECTIONS}
         self.speed = 2
+        self.rotation_speed = math.pi / 2
         self.level = level
+        self.angle = 0.0
+        self.x_angle = math.cos(self.angle)
+        self.y_angle = math.sin(self.angle)
 
     def move(self, direction, start):
-        self.directions[direction] = start
+        if direction in [self.LEFT, self.RIGHT] and start:
+            if direction == self.LEFT:
+                self.angle -= self.rotation_speed
+            if direction == self.RIGHT:
+                self.angle += self.rotation_speed
+            print math.degrees(self.angle)
+            self.x_angle = math.cos(self.angle)
+            self.y_angle = math.sin(self.angle)
+        elif direction in [self.UP, self.DOWN]:
+            self.directions[direction] = start
         # self.level.sound.play_ping(direction)
 
     def update(self):
         old_location = self.rect.copy()
+        old_x_float, old_y_float = self.x_float, self.y_float
         if self.directions[Hero.UP]:
-            self.rect.move_ip(0, -self.speed)
+            self.x_float += self.speed * self.x_angle
+            self.y_float += self.speed * self.y_angle
         if self.directions[Hero.DOWN]:
-            self.rect.move_ip(0, self.speed)
-        if self.directions[Hero.LEFT]:
-            self.rect.move_ip(-self.speed, 0)
-        if self.directions[Hero.RIGHT]:
-            self.rect.move_ip(self.speed, 0)
+            self.x_float -= self.speed * self.x_angle
+            self.y_float -= self.speed * self.y_angle
+
+        self.rect.x, self.rect.y = self.x_float, self.y_float
 
         if spritecollideany(self, self.level.get_walls()):
             self.rect = old_location
+            self.x_float, self.y_float = old_x_float, old_y_float
             self.level.vibrate()
     def is_moving(self):
         return any(self.directions.values())
